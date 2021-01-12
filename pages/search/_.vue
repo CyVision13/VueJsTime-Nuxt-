3f2fcd372e404c6f6c8d2e5da77d1ef08a8999b8
cyvision13 <cyvision13@outlook.com>
2021-01-12 16:04:36 -0500

    queryStrings

--- pages/search/_.vue
+++ pages/search/_.vue
@@ -1,20 +1,23 @@
 <template>
-  <div>
-      <h1>search</h1>
-      <ul>
-          <li v-for="(item,index) in getParams" :key="index">{{item}}</li>
-      </ul>
-  </div>
+<div>
+    <h1>search</h1>
+    <ul>
+        <li v-for="(item,index) in getParams" :key="index">{{item}}</li>
+    </ul>
+</div>
 </template>
 
 <script>
 export default {
-    name:"search",
-    mounted(){
+    name: "search",
+    beforeMount(){
+        console.log('query',this.$route.query);
+    },
+    mounted() {
         console.log(this.$route.params);
     },
-    computed:{
-        getParams(){
+    computed: {
+        getParams() {
             return this.$route.params.pathMatch.split('/')
         }
     }
