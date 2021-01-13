f498eaed2cf39dcf07353c1ecb54e79a763cbf38
cyvision13 <cyvision13@outlook.com>
2021-01-13 01:12:35 -0500

    queryStrings

--- pages/search/_.vue
+++ pages/search/_.vue
@@ -1,6 +1,8 @@
 <template>
 <div>
+    <button @click="pushQuery">push query</button>
     <h1>search</h1>
+    <nuxt-link :to="{name:'product-slug',params:{slug:'samsung-s5'},query:{sorn:true}}">go to product</nuxt-link>
     <ul>
         <li v-for="(item,index) in getParams" :key="index">{{item}}</li>
     </ul>
@@ -15,10 +17,18 @@ export default {
     },
     mounted() {
         console.log(this.$route.params);
+        // setTimeout(()=>{
+        //     this.$router.push({name:'product-slug',params:{slug:'sumsong-s13'}})
+        // },3313)
     },
     computed: {
         getParams() {
             return this.$route.params.pathMatch.split('/')
+        }
+    },
+    methods:{
+        pushQuery(){
+            this.$router.push({query:{search_key:'samsung',brand_id:1}})
         }
     }
 }
