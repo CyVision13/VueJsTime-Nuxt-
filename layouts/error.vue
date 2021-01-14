ab99a9a0a3460b9bd4af59026e5e9a5854cfef51
cyvision13 <cyvision13@outlook.com>
2021-01-14 02:00:27 -0500

    customErrorPage

---
+++ layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>{{error.statusCode}}</h1>
+    <h3>{{error.message}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['error']
+};
+</script>
+
+<style></style>
